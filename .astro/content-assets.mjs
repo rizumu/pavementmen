12a95c28e236353b89e1b4a146531b3fd144cd7a
Thomas Schreiber <rizumu@users.noreply.github.com>
2025-06-18 13:59:02 -0400

    Remove demo cache.

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_VcWn2 from "./metamorphosis.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmetamorphosis%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_trihK from "./x-wing.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-syntax-example%2Findex.md";
-export default new Map([["./metamorphosis.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmetamorphosis%2Findex.md", __ASTRO_IMAGE_IMPORT_VcWn2], ["./x-wing.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-syntax-example%2Findex.md", __ASTRO_IMAGE_IMPORT_trihK]]);
-		
