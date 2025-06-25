2d6adcb328eff34daf703e3c69a7bb6b9a76cb49
Thomas Schreiber <rizumu@users.noreply.github.com>
2025-06-25 14:09:41 -0400

    Try a different youtube shortcode.

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -3,7 +3,15 @@ import React from "react";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-export default function YouTube({ id, title }: { id: string; title?: string }) {
+const Youtube = ({
+  id,
+  title,
+  ...rest
+}: {
+  id: string;
+  title: string;
+  rest: any;
+}) => {
   return (
     <div className="my-4 aspect-video overflow-hidden rounded-2xl">
       <LiteYouTubeEmbed
@@ -14,4 +22,6 @@ export default function YouTube({ id, title }: { id: string; title?: string }) {
       />
     </div>
   );
-}
+};
+
+export default Youtube;
